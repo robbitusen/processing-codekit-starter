b143c2162470db85971c104a9257abc0a60559d2
Jake <jakegabb@gmail.com>
2016-11-18 22:50:02 +0000

    Slightly better default settings, dynamic window size that takes full width and height of viewport

--- source/sketch.js
+++ source/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
-	createCanvas(640, 480);
+	createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
  	ellipse(50, 50, 80, 80);
+}
+
+window.onresize = function() {
+	setup();
 }
